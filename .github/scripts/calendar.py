3423fdd9b7a927c003ca1c4c23f0f790e34ffe85
lingeorge88 <lingeorge04@gmail.com>
2023-10-30 13:58:15 -0700

    removed json key and added error handling to python script

--- .github/scripts/calendar.py
+++ .github/scripts/calendar.py
@@ -9,6 +9,14 @@ import pytz
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
+def get_issue_details():
+    # Dummy function to simulate fetching issue details
+    return {
+        'title': 'Fix bug in application',
+        'due_date': '2023-12-01T10:00:00+00:00',
+        'assignee_email': 'assignee@example.com'
+    }
+
 def create_event(summary, start_time, end_time, attendees):
     """Creates an event in the user's Google Calendar."""
     creds = None
@@ -49,15 +57,21 @@ def create_event(summary, start_time, end_time, attendees):
         print('Event created: %s' % (event.get('htmlLink')))
     except Exception as e:
         print('Error creating the calendar event:', str(e))
+        return False
+    return True
 
 if __name__ == '__main__':
-    # Get the issue details from the GitHub API (you'll need to implement this part)
-    issue_details = get_issue_details()
+    try:
+        # Get the issue details from the GitHub API (you'll need to implement this part)
+        issue_details = get_issue_details()
 
-    # For the purpose of this example, I'm hardcoding these values
-    summary = "Complete Task: " + issue_details['title']
-    start_time = issue_details['due_date']
-    end_time = (datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S%z") + datetime.timedelta(hours=1)).isoformat()
-    attendees = [issue_details['assignee_email']]
+        # For the purpose of this example, I'm hardcoding these values
+        summary = "Complete Task: " + issue_details['title']
+        start_time = issue_details['due_date']
+        end_time = (datetime.datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S%z") + datetime.timedelta(hours=1)).isoformat()
+        attendees = [issue_details['assignee_email']]
 
-    create_event(summary, start_time, end_time, attendees)
+        if not create_event(summary, start_time, end_time, attendees):
+            raise Exception("Failed to create calendar event")
+    except Exception as e:
+        print('An error occurred:', str(e))
